578aa8d93b4b52738eddf1a822aaa202f4d92859
Peter Senna Tschudin <peter.senna@gmail.com>
2012-03-19 18:31:20 -0300

    fixed rcu usage for find_task_by_vpid()

--- yfkm2/source/3.3.0-0.rc5.git3.1.fc17/samples/yfkm2/yfkm2.c
+++ yfkm2/source/3.3.0-0.rc5.git3.1.fc17/samples/yfkm2/yfkm2.c
@@ -56,13 +56,22 @@ DEFINE_RWLOCK(yfkm2_lock);
 int yfkm2_is_pid_running(pid_t pid)
 {
 	struct task_struct *q;
+	int ret;
 
+	rcu_read_lock();
 	q = find_task_by_vpid(pid);
+	if (q != NULL)
+		get_task_struct(q);
+	rcu_read_unlock();
 
 	if (q != NULL && q->pid == pid)
-		return 0;
+		ret = 0;
+	else
+		ret = 1;
 
-	return 1;
+	put_task_struct(q);
+
+	return ret;
 }
 
 /*
@@ -76,14 +85,24 @@ int yfkm2_is_pid_running(pid_t pid)
 int yfkm2_kill(pid_t pid)
 {
 	struct task_struct *q;
+	int ret;
 
+	rcu_read_lock();
 	q = find_task_by_vpid(pid);
+	if (q != NULL)
+		get_task_struct(q);
+		ret = 0;
+	} else
+		ret = 1;
 
-	if (q != NULL) {
+	rcu_read_unlock();
+
+	if (ret == 0) {
 		force_sig(SIGKILL, q);
-		return 0;
+		put_task_struct(q);
 	}
-	return 1;
+
+	return ret;
 }
 
 /*
@@ -204,7 +223,6 @@ asmlinkage long sys_yfkm2_notifyme(pid_t monitor, pid_t notifyme)
 		}
 
 		return 0;
-	} else {
+	} else
 		return 1;
-	}
 }
