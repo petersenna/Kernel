8d8b0632240b100e6c7f5dad68164b00501dfdf7
Peter Senna Tschudin <peter.senna@gmail.com>
2012-03-23 18:22:27 -0300

    rcu fix 3

--- yfkm2/source/3.3.0-0.rc5.git3.1.fc17/samples/yfkm2/yfkm2.c
+++ yfkm2/source/3.3.0-0.rc5.git3.1.fc17/samples/yfkm2/yfkm2.c
@@ -56,22 +56,15 @@ DEFINE_RWLOCK(yfkm2_lock);
 int yfkm2_is_pid_running(pid_t pid)
 {
 	struct task_struct *q;
-	int ret;
 
 	rcu_read_lock();
 	q = find_task_by_vpid(pid);
-	if (q != NULL)
-		get_task_struct(q);
 	rcu_read_unlock();
 
 	if (q != NULL && q->pid == pid)
-		ret = 0;
+		return 0;
 	else
-		ret = 1;
-
-	put_task_struct(q);
-
-	return ret;
+		return 1;
 }
 
 /*
@@ -89,17 +82,10 @@ int yfkm2_kill(pid_t pid)
 
 	rcu_read_lock();
 	q = find_task_by_vpid(pid);
-	if (q != NULL) {
-		get_task_struct(q);
-		ret = 0;
-	} else
-		ret = 1;
-
 	rcu_read_unlock();
 
-	if (ret == 0) {
+	if (q != NULL) {
 		force_sig(SIGKILL, q);
-		put_task_struct(q);
 	}
 
 	return ret;
